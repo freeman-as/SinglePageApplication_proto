608d538cc79675abe8999452c1949dc1908a4c34
Atsunori Sagawa <freeman.by2018@gmail.com>
2019-01-06 16:09:08 +0900

    [Modify] chatオブジェクトの実装

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -22,49 +22,48 @@ spa.fake = (function () {
         return 'id_' + String(fakeIdSerial++);
     };
 
-    getPeopleList = function () {
-        return [
-            {
-                name: 'Betty',
-                _id: 'id_01',
-                css_map: {
-                    top: 20,
-                    left: 20,
-                    'background-color': 'rgb(128, 128, 128)',
-                }
-            },
-            {
-                name: 'Mike',
-                _id: 'id_02',
-                css_map: {
-                    top: 60,
-                    left: 20,
-                    'background-color': 'rgb(128, 255, 128)',
-                }
-            },
-            {
-                name: 'Pebbles',
-                _id: 'id_03',
-                css_map: {
-                    top: 100,
-                    left: 20,
-                    'background-color': 'rgb(128, 192, 192)',
-                }
-            },
-            {
-                name: 'Wilma',
-                _id: 'id_04',
-                css_map: {
-                    top: 140,
-                    left: 20,
-                    'background-color': 'rgb(192, 128, 128)',
-                }
-            },
-        ]
-    };
+    peopleList = [
+        {
+            name: 'Betty',
+            _id: 'id_01',
+            css_map: {
+                top: 20,
+                left: 20,
+                'background-color': 'rgb( 128, 128, 128)',
+            }
+        },
+        {
+            name: 'Mike',
+            _id: 'id_02',
+            css_map: {
+                top: 60,
+                left: 20,
+                'background-color': 'rgb( 128, 255, 128)',
+            }
+        },
+        {
+            name: 'Pebbles',
+            _id: 'id_03',
+            css_map: {
+                top: 100,
+                left: 20,
+                'background-color': 'rgb( 128, 192, 192)',
+            }
+        },
+        {
+            name: 'Wilma',
+            _id: 'id_04',
+            css_map: {
+                top: 140,
+                left: 20,
+                'background-color': 'rgb( 192, 128, 128)',
+            }
+        }
+    ];
 
     mockSio = (function () {
         var on_sio, emit_sio,
+            send_listchange, listchange_idto,
             callback_map = {};
 
         on_sio = function(msg_type, callback) {
@@ -72,20 +71,40 @@ spa.fake = (function () {
         };
 
         emit_sio = function(msg_type, data) {
+            var person_map;
+
             // 3秒間の遅延後に[userupdate]コールバックで[adduser]イベントに応答する
             // 
             if (msg_type === 'adduser' && callback_map.userupdate) {
                 setTimeout(function () {
-                    callback_map.userupdate(
-                        [{
-                            _id: makeFakeId(),
-                            name: data.name,
-                            css_map: data.css_map,
-                        }]
-                    );
+                    person_map = {
+                        _id: makeFakeId(),
+                        name: data.name,
+                        css_map: data.css_map,
+                    };
+                    peopleList.push(person_map);
+                    callback_map.userupdate([person_map]);
                 }, 3000);
             }
         };
+
+        // 1秒に1回listchangeコールバックを使うようにする
+        // 一度成功したら止める
+        // 
+        send_listchange = function () {
+            listchange_idto = setTimeout(function () {
+                if (callback_map.listchange) {
+                    callback_map.listchange([peopleList]);
+                    listchange_idto = undefined;
+                }
+                else {
+                    send_listchange();
+                }
+            }, 1000);
+        };
+
+        // 処理を開始する必要がある
+        send_listchange();
 
         return {
             emit: emit_sio,
@@ -94,7 +113,6 @@ spa.fake = (function () {
     }());
 
     return {
-        getPeopleList: getPeopleList,
         mockSio: mockSio,
     };
 }());
--- js/spa.model.js
+++ js/spa.model.js
@@ -24,6 +24,7 @@ spa.model = (function () {
             people_cid_map: {},
             people_db: TAFFY(),
             user: null,
+            is_connected: false,
         },
 
         isFakeData = true,
@@ -32,7 +33,8 @@ spa.model = (function () {
         makePerson, removePerson, people, initModule;
 
     // peopleオブジェクトAPI
-    // TODO API仕様書く
+    // --------------------
+    // TODO: API仕様書く
     // 
     personProto = {
         get_is_user: function () {
@@ -170,6 +172,86 @@ spa.model = (function () {
         };
     }());
 
+    // chatオブジェクトAPI
+    // --------------------
+    // TODO: API仕様書く
+    // 
+    chat = (function () {
+        var _publish_listchange, _update_list, _leave_chat, 
+            join_chat;
+
+        // 内部メソッド開始
+        _update_list = function(arg_list) {
+            var i, person_map, make_person_map,
+                people_list = arg_list[0];
+
+            clearPeopleDb();
+
+            PERSON:
+            for (i = 0; i < people_list.length; i++) {
+                person_map = people_list[i];
+
+                if (! person_map.name) {
+                    continue PERSON;
+                }
+
+                // ユーザーを特定したらcss_mapを更新して残りを飛ばす
+                if (stateMap.user && stateMap.user.id === person_map._id) {
+                    stateMap.user.css_map = person_map.css_map;
+                    continue PERSON;
+                }
+
+                make_person_map = {
+                    cid: person_map._id,
+                    css_map: person_map.css_map,
+                    id: person_map._id,
+                    name: person_map.name,
+                };
+
+                makePerson(make_person_map);
+            }
+
+            stateMap.people_db.sort('name');
+        };
+
+        _publish_listchange = function(arg_list) {
+            _update_list(arg_list);
+            $.gevent.publish('spa-listchange', [arg_list]);
+        };
+
+        // パブリックメソッド開始
+        _leave_chat = function() {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            stateMap.is_connected = false;
+            if (sio) {
+                sio.emit('leavechat');
+            }
+        };
+
+        join_chat = function() {
+            var sio;
+
+            if (stateMap.is_connected) {
+                return false;
+            }
+
+            if (stateMap.user.get_is_anon()) {
+                console.warn('User must be defined before joining chat.');
+                return false;
+            }
+
+            sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            sio.on('listchange', _publish_listchange);
+            stateMap.is_connected = true;
+            return true;
+        };
+
+        return {
+            _leave: _leave_chat,
+            join: join_chat,
+        };
+    }());
+
     initModule = function () {
         var i, people_list, person_map;
 
@@ -197,6 +279,7 @@ spa.model = (function () {
 
     return {
         initModule: initModule,
+        chat: chat,
         people: people,
     };
 }());
